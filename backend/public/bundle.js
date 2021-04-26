5d6a46a2e093d2ac76f238f3f237368a30f16614
valentinaQuiroga <valequiroga87@gmail.com>
2021-04-26 22:47:16 +0100

    bundles and webpack

---
+++ backend/public/bundle.js
@@ -0,0 +1,1 @@
+(()=>{var e={416:(e,r,t)=>{"use strict";t.r(r)}},r={};function t(o){var i=r[o];if(void 0!==i)return i.exports;var n=r[o]={exports:{}};return e[o](n,n.exports,t),n.exports}t.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},alert("it works!"),t(416)})();
